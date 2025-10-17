061bba55b00c614782de47e027e6de9a099ff83e
Mishardo <david04veron@gmail.com>
2025-10-17 15:26:39 -0300

    altaProducto() actualizado, falta terminar
    Agrege dos funciones para la validacion de la fecha

--- main.c
+++ main.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <ctype.h>
 #include "funciones.h"
+#include <time.h>
+
+#define IVA 23
 
 // STRUCT
 typedef struct
 {
     int orden;
     char vendedor[20];
-    char fecha[10];
+    int dia, mes, anio;
     char categoria[15];
     int cantidad;
     float precio;
@@ -16,7 +19,7 @@ typedef struct
     float iva;
     float total;
     int activo;
-} calzados;
+} Calzados;
 
 //---------------------FUNCIONES EXTRA--------------------------------
 
@@ -26,6 +29,43 @@ void limpiarBuffer()
 {
     char c;
     while ((c = getchar()) != '\n' && c != EOF);
+}
+
+void obtenerFechaActual(int *dia, int *mes, int *anio)
+{
+    time_t t = time(NULL);
+    struct tm *fechaLocal = localtime(&t);
+
+    *dia = fechaLocal->tm_mday;
+    *mes = fechaLocal->tm_mon + 1;      // Los meses van de 0 a 11, por eso +1
+    *anio = fechaLocal->tm_year + 1900; // Años desde 1900
+}
+
+void validarFecha(Calzados *cal, int dia_actual, int mes_actual, int anio_actual)
+{
+    while (cal->anio > anio_actual)
+    {
+        printf("El anio ingresado no es correcto.\n");
+        printf("Vuelva a intentarlo: ");
+        scanf("%d", cal->anio);
+        limpiarBuffer();
+    }
+
+    while (cal->mes > mes_actual)
+    {
+        printf("El mes ingresado no es correcto.\n");
+        printf("Intente de nuevo: ");
+        scanf("%d", cal->mes);
+        limpiarBuffer();
+    }
+
+    while (cal->dia > dia_actual)
+    {
+        printf("El mes ingresado no es correcto.\n");
+        printf("Intente de nuevo: ");
+        scanf("%d", cal->dia);
+        limpiarBuffer();
+    }
 }
 
 // ---------------------------FUNCIONES PEDIDAS--------------------------
@@ -50,21 +90,92 @@ void crearBinario(FILE *archivo)
 // 3 (ALTA)
 void altaProducto(FILE *archivo)
 {
-    calzados cal;
+    Calzados cal;
     archivo = fopen("registros.dat", "r+b");
+    int dia_actual, mes_actual, anio_actual;
 
+    // °Ingreso de datos y validaciones
     printf("Ingrese el nro de orden (mayor a 0): ");
     scanf("%d", &cal.orden);
-
     limpiarBuffer();
+
+    // -Validar que el nro de orden ingresado no exista previamente-
+    // -Validar que no haya ingresado un valor flotante-????
+    // -Validar que no haya ingresado un caracter-????
 
     while (cal.orden <= 0)
     {
-        printf("El numero ingresado es erroneo.\n");
+        printf("El numero ingresado no puede ser igual o menor que 0.\n");
         printf("Vuelva a intentarlo: ");
         scanf("%d", &cal.orden);
         limpiarBuffer();
     }
+
+    printf("Nombre del vendedor/a: ");
+    scanf("%s", cal.vendedor);
+    limpiarBuffer();
+
+    // -Vendría una validación-
+
+    printf("Fecha de ingreso del producto (No puede superar la fecha actual): ");
+    scanf("%d %d %d", &cal.dia, &cal.mes, &cal.anio);
+    limpiarBuffer();
+
+    obtenerFechaActual(&dia_actual, &mes_actual, &anio_actual);
+    validarFecha(&cal, dia_actual, mes_actual, anio_actual);
+
+    printf("Ahora, ingrese la categoria del calzado: ");
+    scanf("%s", cal.categoria);
+
+    // -Vendría una validación-
+
+    printf("Ingrese la cantidad: ");
+    scanf("%d", &cal.cantidad);
+    limpiarBuffer();
+
+    while (cal.cantidad < 0)
+    {
+        printf("Cantidad ingresada inferior a cero, ingreso invalido.\n");
+        printf("Intente de nuevo: ");
+        scanf("%d", &cal.cantidad);
+        limpiarBuffer();
+    }
+
+    printf("Ingrese el precio unitario: ");
+    scanf("%f", &cal.precio);
+    limpiarBuffer();
+
+    while (cal.precio <= 0)
+    {
+        printf("El precio ingresado no puede ser igual o inferior a 0.\n");
+        printf("Ingrese de nuevo: ");
+        scanf("%f", &cal.precio);
+        limpiarBuffer();
+    }
+
+    printf("Ingrese el descuento por unidad (en porcentaje del 0 al 100 sin incluirlo): ");
+    scanf("%f", &cal.descuento);
+    limpiarBuffer();
+
+    while (cal.descuento < 0 || cal.descuento >= 100)
+    {
+        printf("El descuento ingresado es incorrecto.\n");
+        printf("Intente de nuevo: ");
+        scanf("%f", &cal.descuento);
+        limpiarBuffer();
+    }
+
+    cal.activo = 1; // Valido que el producto está activo
+
+    // °Cuentas
+    cal.sub_total = cal.cantidad * cal.precio - cal.cantidad * cal.descuento;
+    cal.iva = cal.sub_total * IVA;
+    cal.total = cal.sub_total + cal.iva;
+
+    // °Agregar datos al archivo
+    fseek(archivo, (cal.orden - 1) * sizeof(Calzados), SEEK_SET);
+    fwrite(&cal, sizeof(Calzados), 1, archivo);
+    fclose(archivo);
 
     // *************FALTA TERMINAR*****************
 }
