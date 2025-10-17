e3fae523f67bec0da27f2a6fa530e8e1ed6028ad
Mishardo <david04veron@gmail.com>
2025-10-17 00:00:54 -0300

    archivo principal

---
+++ main.c
@@ -0,0 +1,30 @@
+//FUNCIONES (Despues las pasamos a un archivo a parte donde estáran todas las funciones)--------------------------
+
+//1 (MENU)
+
+//2 (CREAR ARCHIVO)
+
+//3 (ALTA)
+
+//4 (LISTAR BINARIO)
+
+//5 (BUSCAR)
+
+//6 (MODIFICAR)
+
+//7 (BAJA LOGICA)
+
+//8 (BAJA FISICA)
+
+//9 (LISTAR TEXTO)
+
+//-------------------------------------------------------------------------------------
+
+
+int main()
+{
+    //MENU
+
+
+    return 0;
+}
